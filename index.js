b859ae50aa0fbfceb87236df05daad3f3c628962
George Chen <georgechen@Georges-MacBook-Air-3.local>
2019-09-25 17:01:40 -0700

    connect to postgres and create model

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3005;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 
 app.listen(port, () => console.log(`App listening on ${port}!`))
---
+++ models/repo.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../database/index.js');
+
+const Repo = sequelize.define('repo', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.STRING
+  },
+  language: {
+    type: Sequelize.STRING
+  },
+  forks: {
+    type: Sequelize.INTEGER
+  }
+})
+
+module.exports = Repo;
