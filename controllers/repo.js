d6f640c5ffd2ccddd64f7a65e5071572ae19813e
George Chen <georgechen@Georges-MacBook-Air-3.local>
2019-09-26 17:25:32 -0700

    successfully search for a repo with parameters

--- controllers/repo.js
+++ controllers/repo.js
@@ -2,34 +2,17 @@ const Repo = require('../models/repo.js');
 
 const saveRepo = async (results) => {
   if (results.length === 0) throw new Error('no repos to save');
-
-  // for (var i = 0; i < results.length; i++) {
-  //   let result = results[i];
-  //   let parsedResult = {
-  //     name: result.name,
-  //     description: result.description,
-  //     language: result.language,
-  //     forks: result.forks
-  //   }
-  //   let repo = await Repo.build(parsedResult);
-  //   await repo.save()
-  //       .then(() => {
-  //         console.log('new task saved');
-  //       })
-  //       .catch(() => {
-  //         console.log('failed to save repos')
-  //       })
-  // }
-  // let repo = await Repo.build(parsedResult);
   await Repo.bulkCreate(results);
-  console.log('saved to database')
+  console.log('saved to database');
 
 }
 
-const getRepos = async () => {
-  let repo = await Repo.findAll();
-  console.log('ran')
-  return repo; 
+const searchOrGetAllRepos = async (queries) => {
+  let repos = await Repo.findAll({
+    where: queries
+  })
+  return repos;
 }
 
-module.exports = {saveRepo, getRepos};
+
+module.exports = {saveRepo, searchOrGetAllRepos};
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Repo = require('./models/repo.js');
 const githubAPI = require('./util/api.js');
 const fn = require('./controllers/repo.js');
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //make http request to github to grab list of repos
@@ -15,16 +16,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 //create GET request endpoint to retrieve all Repos
 app.get('/repos', async (req, res) => {
   try {
-    let repos = await fn.getRepos();
+    let repos = await fn.searchOrGetAllRepos(req.query);
     res.status(200).send(repos); 
   }
   catch (err) {
-    res.status(400).send();
+    res.status(400).send(err);
   }
 })
 
 //create GET request endpoint to retrieve filtered parameters (include a search paramater)
-
+//search by repo name 
+//search by language
+// app.get('/repos/search', async (req, res) => {
+//   try {
+//     console.log(req.body);
+//   }
+//   catch (err) {
+//     console.log(err);
+//   }
+// })
 
 
 const main = async () => {
